261258d840d6116332b30d67b141b89040c33991
DrX <48512644+Dichill@users.noreply.github.com>
2020-08-09 21:26:19 +0800

    Create dummy.py

---
+++ dummy.py
@@ -0,0 +1,18 @@
+import importlib
+api = importlib.import_module('mcsrvstatAPI')
+
+mcsrv = api.mcserverstatus()
+server = "mc.hypixel.net"
+
+print("Server: " + server)
+print("IP: " + mcsrv.getIP(server))
+if mcsrv.getStatus(server) == True:
+    print("Status: Online")
+elif mcsrv.getStatus(server) == False:
+    print("Status: Offline")
+else:
+    print("Error")
+
+print("MOTD: " + mcsrv.getMotd(server, 'html'))
+print("Players: "+ str(mcsrv.getPlayers(server, 'online')) + "/" + str(mcsrv.getPlayers(server, 'max')))
+print("Hostname: " + mcsrv.getHostname(server))
